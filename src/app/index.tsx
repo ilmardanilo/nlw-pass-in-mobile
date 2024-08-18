e942cc8efc7c0b1e3c9d15c7cd085afd5f937365
ilmar <ilmar.danilo@querodelivery.com>
2024-08-17 21:07:58 -0300

    feat: configure tailwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,9 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <View style={style.container}>
-      <Text style={style.title}>Hello React Native!</Text>
+    <View className="flex-1 bg-green-500 items-center justify-center">
+      <Text className="text-white ">Hello React Native!</Text>
     </View>
   );
 }
-
-const style = StyleSheet.create({
-  container: {
-    backgroundColor: '#191724',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: { color: '#e0def4', fontSize: 32 },
-});
