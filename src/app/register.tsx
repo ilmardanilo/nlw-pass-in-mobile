7d759c93c991b0c2c6f7214c75f186191208ce0e
ilmar <ilmar.danilo@querodelivery.com>
2024-08-18 20:59:35 -0300

    feat: get name and email field and handle register

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
+import { Alert, Image, StatusBar, View } from 'react-native';
 import { FontAwesome6, MaterialIcons } from '@expo/vector-icons';
-import { Image, StatusBar, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 
 import { colors } from '@/styles/colors';
 
@@ -8,6 +9,17 @@ import { Input } from '@/components/input';
 import { Button } from '@/components/button';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleRegister() {
+    if (!name.trim() || !email.trim()) {
+      return Alert.alert('Inscrição', 'Preencha todos os campos!');
+    }
+
+    router.push('/ticket');
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle={'light-content'} />
@@ -25,7 +37,7 @@ export default function Register() {
             size={20}
             color={colors.green[200]}
           />
-          <Input.Field placeholder="Nome completo" />
+          <Input.Field placeholder="Nome completo" onChangeText={setName} />
         </Input>
 
         <Input>
@@ -35,10 +47,14 @@ export default function Register() {
             color={colors.green[200]}
           />
           {/* keyboardType nem todos são cross platform, deve verificar na docs */}
-          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+          <Input.Field
+            placeholder="E-mail"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+          />
         </Input>
 
-        <Button title="Realizar inscrição" />
+        <Button title="Realizar inscrição" onPress={handleRegister} />
 
         <Link
           href="/"
