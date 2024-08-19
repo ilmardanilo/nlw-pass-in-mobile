f3201a8aa01b90b3837aaf0003d65ee9ff4217fa
ilmar <ilmar.danilo@querodelivery.com>
2024-08-19 15:35:43 -0300

    feat: add animation in credential

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -4,8 +4,10 @@ import {
   Image,
   ImageBackground,
   TouchableOpacity,
+  useWindowDimensions,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { MotiView } from 'moti';
 
 import { colors } from '@/styles/colors';
 
@@ -20,8 +22,31 @@ interface IProps {
 }
 
 export function Credential({ data, onChangeAvatar, onExpandQRCode }: IProps) {
+  const { height } = useWindowDimensions();
+
   return (
-    <View className="w-full self-stretch items-center">
+    <MotiView
+      className="w-full self-stretch items-center"
+      from={{
+        opacity: 0,
+        translateY: -height,
+        rotateZ: '50deg',
+        rotateY: '30deg',
+        rotateX: '30deg',
+      }}
+      animate={{
+        opacity: 1,
+        translateY: 0,
+        rotateZ: '0deg',
+        rotateY: '0deg',
+        rotateX: '0deg',
+      }}
+      transition={{
+        type: 'spring',
+        damping: 20,
+        rotateZ: { damping: 15, mass: 3 },
+      }}
+    >
       <Image
         source={require('@/assets/ticket/band.png')}
         className="w-24 h-52 z-10"
@@ -79,6 +104,6 @@ export function Credential({ data, onChangeAvatar, onExpandQRCode }: IProps) {
           </Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </MotiView>
   );
 }
